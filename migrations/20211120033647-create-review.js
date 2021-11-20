8b4732b5144cbac4dc6c99752ec182e4ab3ac771
Allen <jihoshin.28@gmail.com>
2021-11-19 19:38:13 -0800

    review model

---
+++ migrations/20211120033647-create-review.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Reviews', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      subject: {
+        type: Sequelize.STRING
+      },
+      subject: {
+        type: Sequelize.STRING
+      },
+      text: {
+        type: Sequelize.TEXT
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      rating: {
+        type: Sequelize.INTEGER
+      },
+      city: {
+        type: Sequelize.STRING
+      },
+      state: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Reviews');
+  }
+};
