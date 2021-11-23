f8726838809d99a5b8503ee15e812d46871542e7
Allen <jihoshin.28@gmail.com>
2021-11-22 23:07:04 -0800

    reviews backbone

--- controllers/review.js
+++ controllers/review.js
@@ -1,50 +1,45 @@
 const models = require('../models')
 
 const getAllReviews = async(req, res) => {
-    console.log(req.params.id)
-    await models.Review.findAll({}, (err, reviews) => {
-        if(err) {
-            return res.send(err).status(500)
-        }
-        res.json(reviews).status(200)
-    })
+    let reviews = await models.Review.findAll()
+    if(reviews.length === 0){
+        return res.json({"status": "There are no reviews."}).status(200)
+    }
+    return res.json(reviews).status(200)
 }
 
 const getReview = async(req, res) => {
     console.log(req.params.id)
-    await models.Review.findOne({_id: req.params.id}, (err, review) => {
-        if(err) {
-            return res.send(err).status(500)
+    let review = await models.Review.findOne({
+        where: {
+            id: req.params.id
         }
-        res.json(review).status(200)
     })
+
+    if(!review){
+        return res.json({"error": "Review not found"}).status(200)
+    } else {
+        return res.json(review).status(200)
+    }
+
 }
 
 const createReview = async(req, res) => {
-    await models.Review.create(req.body.ReviewInfo, (err, newReview) => {
-        if(err){
-            return res.send(err).status(500)
-        }
-        res.json(newReview).status(200)
-    })
+    console.log(req.body.reviewInfo)
+    const newReview = await models.Review.create(req.body.reviewInfo)
+    // need to add validator logic
+    console.log(newReview)
+    res.json(newReview).status(200)
 }
 
 const updateReview = async(req, res) => {
-    await models.Review.update(req.body.ReviewInfo, (err, updatedReview) => {
-        if(err){
-            return res.send(err).status(500)
-        }
-        res.json(updatedReview).status(200)
-    })
+    const updatedReview = await models.Review.update(req.body.reviewInfo)
+    console.log(updatedReview)
+    res.json(updatedReview).status(200)
 }
 
 const deleteReview = async(req, res) => {
-    await models.Review.delete(req.body.ReviewInfo, (err, deletedReview) => {
-        if(err){
-            return res.send(err).status(500)
-        }
-        res.json(deletedReview).status(200)
-    })
+    // await models.Review.destroy(req.params.id)
 }
 
 module.exports = {
