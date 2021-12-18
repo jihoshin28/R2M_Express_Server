9a549a8e89780e5adacb2e6c65197f557bd39dc5
Allen <jihoshin.28@gmail.com>
2021-12-18 13:44:20 -0800

    server port

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ app.use(express.json())
 app.use(cors())
 app.use('/', routes)
 
-const PORT = 3000 | process.env.PORT
+const PORT = process.env.PORT || 3000 
                       
 app.listen(PORT, () => console.log(`Listening on Port ${PORT}`));
