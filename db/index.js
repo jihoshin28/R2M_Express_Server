bf5efa76338c42af5f8ca9cbf8c9245dcbb2cb56
Allen <jihoshin.28@gmail.com>
2021-12-20 19:50:37 -0800

    psql

--- db/index.js
+++ db/index.js
@@ -1,15 +1,24 @@
 const Sequelize = require('sequelize')
 const sequelize = new Sequelize({
-    "url": "postgres://qmsslupljiwdog:cab52ea33ebaa669601bc4426024af19a40c4608990c28de6fd195b55cc0c4b5@ec2-34-204-127-36.compute-1.amazonaws.com:5432/dfodqifg0o195a",
-    dialect: 'postgres',
-    operatorAliases: false,
-    pool: {
-        max: 10,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
-})
+    database: "dfodqifg0o195a",
+    username: "qmsslupljiwdog",
+    password: "cab52ea33ebaa669601bc4426024af19a40c4608990c28de6fd195b55cc0c4b5",
+    host: "ec2-34-204-127-36.compute-1.amazonaws.com",
+    port: 5432,
+    dialect: "postgres",
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+     },
+     pool: {
+         max: 10,
+         min: 0,
+         acquire: 30000,
+         idle: 10000
+     }
+});
 // async function tryConnection () {
 //     try {
 //         await sequelize.authenticate();
