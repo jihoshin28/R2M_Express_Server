64214efdbf2d1c1e9acde1bfc956e0de3f6da720
Allen <jihoshin.28@gmail.com>
2021-12-20 19:46:16 -0800

    psql

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize('upack_haulers', 'allen', 'jeeho123', {
-    host: 'localhost',
+const sequelize = new Sequelize({
+    "url": "postgres://qmsslupljiwdog:cab52ea33ebaa669601bc4426024af19a40c4608990c28de6fd195b55cc0c4b5@ec2-34-204-127-36.compute-1.amazonaws.com:5432/dfodqifg0o195a",
     dialect: 'postgres',
     operatorAliases: false,
     pool: {
