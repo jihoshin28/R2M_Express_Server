47694358fb9e11cf1f8808f3cdc96886dadf8e9b
Allen <jihoshin.28@gmail.com>
2021-12-22 19:19:12 -0800

    added concurrently

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
-app.use(cors({origin: ['http://localhost:3000', 'https://intense-oasis-88289.herokuapp.com']}))
+app.use(cors())
 app.use('/', routes)
 
 const PORT = process.env.PORT || 3000 
